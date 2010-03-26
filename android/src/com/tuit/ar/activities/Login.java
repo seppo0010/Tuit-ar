16ba37bec8940d193aa2586f0cd0dd9ea20f3d4d
Sebastian <seppo0010@gmail.com>
2010-03-26 00:59:56 -0300

    login / Usability

--- android/src/com/tuit/ar/activities/Login.java
+++ android/src/com/tuit/ar/activities/Login.java
@@ -3,12 +3,15 @@ package com.tuit.ar.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.tuit.ar.R;
 import com.tuit.ar.activities.timeline.Friends;
@@ -20,6 +23,7 @@ import com.tuit.ar.api.request.Options;
 public class Login extends Activity implements TwitterObserver {
 	private EditText username;
 	private EditText password;
+	private Button login;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,16 +36,26 @@ public class Login extends Activity implements TwitterObserver {
 		username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
-        Button login = (Button) findViewById(R.id.login);
+        login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) { login(); }
 		});
 
+        password.setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				login();
+				return true;
+			}
+		});
         Twitter.getInstance().addObserver(this);
     }
 
     private void login() {
+    	username.setEnabled(false);
+    	password.setEnabled(false);
+    	login.setEnabled(false);
         Twitter twitter = Twitter.getInstance();
         twitter.setUsername(username.getText().toString());
         twitter.setPassword(password.getText().toString());
@@ -54,6 +68,9 @@ public class Login extends Activity implements TwitterObserver {
     private void loginFailed() {
         Twitter twitter = Twitter.getInstance();
         twitter.clearCredentials();
+    	username.setEnabled(true);
+    	password.setEnabled(true);
+    	login.setEnabled(true);
 		Toast.makeText(this.getApplicationContext(), this.getString(R.string.unableToVerifyCredentials), Toast.LENGTH_LONG).show();
     }
 
