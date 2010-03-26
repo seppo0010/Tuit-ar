0b4df855b0bddf1bcec4e3f19014d19fa44438fa
Sebastian Waisbrot <seppo0010@gmail.com>
2010-03-26 13:10:54 -0300

    Removed unused import

--- android/src/com/tuit/ar/models/Timeline.java
+++ android/src/com/tuit/ar/models/Timeline.java
@@ -2,7 +2,6 @@ package com.tuit.ar.models;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
