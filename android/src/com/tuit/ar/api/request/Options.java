87ec50d3e442ebb16eec958f5ee383d8171c1240
Sebastian Waisbrot <seppo0010@gmail.com>
2010-05-24 13:41:39 -0300

    Added unique request idetnification

--- android/src/com/tuit/ar/api/request/Options.java
+++ android/src/com/tuit/ar/api/request/Options.java
@@ -5,21 +5,47 @@ public enum Options {
 		public String toString() {
 			return "account/verify_credentials";
 		}
+
+		@Override
+		public boolean mustBeUnique() {
+			return false;
+		}
 	}, FRIENDS_TIMELINE {
 		public String toString() {
 			return "statuses/home_timeline";
+		}
+
+		@Override
+		public boolean mustBeUnique() {
+			return true;
 		}
 	}, REPLIES_TIMELINE {
 		public String toString() {
 			return "statuses/mentions";
 		}
+
+		@Override
+		public boolean mustBeUnique() {
+			return true;
+		}
 	}, POST_TWEET {
 		public String toString() {
 			return "statuses/update";
+		}
+
+		@Override
+		public boolean mustBeUnique() {
+			return false;
 		}
 	}, POST_TWEET_WITH_PHOTO {
 		public String toString() {
 			return "statuses/update_with_photo";
 		}
-	}
+
+		@Override
+		public boolean mustBeUnique() {
+			return false;
+		}
+	};
+	abstract boolean mustBeUnique();
 }
