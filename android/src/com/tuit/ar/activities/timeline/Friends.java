5ce8c18c5e58d785fd829c5932134573b6d7e96d
Sebastian Waisbrot <seppo0010@gmail.com>
2010-06-07 21:55:58 -0300

    First run "lazy mode" warning

--- android/src/com/tuit/ar/activities/timeline/Friends.java
+++ android/src/com/tuit/ar/activities/timeline/Friends.java
@@ -1,15 +1,44 @@
 package com.tuit.ar.activities.timeline;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 
 import com.tuit.ar.R;
 import com.tuit.ar.api.Twitter;
+import com.tuit.ar.models.Settings;
 
 public class Friends extends com.tuit.ar.activities.timeline.Status {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(getString(R.string.friends));
+		Settings settings = Settings.getInstance();
+		if (!settings.getSharedPreferences(this).contains(Settings.LAZY_MODE)) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(R.string.firstRun);
+			builder.setMessage(R.string.firstRunMessage);
+			builder.setPositiveButton(R.string.yes, new OnClickListener() {
+				public void onClick(DialogInterface arg0, int arg1) {
+					getTimeline().refresh();
+					Settings settings = Settings.getInstance();
+					Editor editor = settings.getSharedPreferences(Friends.this).edit();
+					editor.putBoolean(Settings.LAZY_MODE, false);
+					editor.commit();
+				}
+			});
+			builder.setNegativeButton(R.string.no, new OnClickListener() {
+				public void onClick(DialogInterface arg0, int arg1) {
+					Settings settings = Settings.getInstance();
+					Editor editor = settings.getSharedPreferences(Friends.this).edit();
+					editor.putBoolean(Settings.LAZY_MODE, true);
+					editor.commit();
+				}
+			});
+			builder.show();
+		}
 	}
 
 	@Override  
